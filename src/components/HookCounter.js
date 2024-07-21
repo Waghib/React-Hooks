7dc8c894e51804fca1670637e62278b3abb77ec4
Waghib <Waghibahmad30@gmail.com>
2024-07-21 16:15:22 +0500

    rules of hooks added

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -16,3 +16,7 @@ export default HookCounter
 // step 1: create a functional componenet
 // step 2: initialize with 0
 // step 3: implement a function
+
+// Rules of Hooks
+// Only call hooks at the top level : dont call hooks inside loops, conditions, or nested functions
+// Only call Hooks from react funtions : Call them from within react functional componenets and not just any regular js function
