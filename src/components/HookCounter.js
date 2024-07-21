63cedac71e2a2ca039b8c6e457fea52162f6a50e
Waghib <Waghibahmad30@gmail.com>
2024-07-21 16:08:13 +0500

    useState Hook (part 2)

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function HookCounter() {
+
+  const  [count, setCount] = useState(0) //array destructuring
+
   return (
-    <div>HookCounter</div>
+    <div>
+      <button onClick={() => setCount(count + 1)}>Count {count}</button>
+    </div>
   )
 }
 
 export default HookCounter
+
+// step 1: create a functional componenet
+// step 2: initialize with 0
+// step 3: implement a function
