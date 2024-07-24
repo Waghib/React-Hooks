40a2ec85cbb5dd939f3bd3609ea02d202ae761dd
Waghib <Waghibahmad30@gmail.com>
2024-07-24 22:28:02 +0500

    conditionally run useEffect

--- src/components/HookCounterOne.js
+++ src/components/HookCounterOne.js
@@ -2,14 +2,19 @@ import React, {useState, useEffect} from 'react'
 
 function HookCounterOne() {
 
-   const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0)
+  const [name, setName] = useState('')
+
    useEffect(() => {
-       document.title = `You clicked ${count} times`
-   }  )
+
+    console.log('useEffect - Updating document title')
+    document.title = `You clicked ${count} times`
+   }, [count]) // second parameter is an array of values that useEffect depends on. If the value of count changes, useEffect will be called.
 
   return (
     <div>
-        <button onClick={() => setCount(count + 1)}>Count {count}</button>
+      <input type="text" value={name} onChange={e => setName(e.target.value)} />
+      <button onClick={() => setCount(count + 1)}>Count {count}</button>
     </div>
   )
 }
