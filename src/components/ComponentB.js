b27194e9710564720e7d410eff68a411388b3a0b
Waghib <Waghibahmad30@gmail.com>
2024-07-29 01:08:22 +0500

    useReducer with useContext

---
+++ src/components/ComponentB.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComponentD from './ComponentE'
+
+function ComponentB() {
+  return (
+    <div>
+        <ComponentD />
+    </div>
+  )
+}
+
+export default ComponentB
